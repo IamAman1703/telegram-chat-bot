c66aa64585a71f2c68ec5707e8f9f49ca849dbd1
IamAman1703 <77916532+IamAman1703@users.noreply.github.com>
2021-12-08 16:40:28 +0530

    Update Responses.py

--- Responses.py
+++ Responses.py
@@ -32,5 +32,5 @@ def sample_responses(input_text):
             count = count + 1
         return str(news_1)
     if pol<0:
-        return "CHii gande insaan"
+        return "Sorru No offensive language !!"
     return "I dont get you !!"
